2cead31deff096f1c871f61e0c51b53ea942ffd6
Yury Bialykh <Yury_Bialykh@epam.com>
2013-01-22 15:18:24 +0300

    office spec settings

---
+++ system-spec/EPBYGROW0105.el
@@ -0,0 +1,7 @@
+;; Home Desktop specific configurations
+
+;; Cache 
+(setq cache-dir (expand-file-name "emacs-cache" dotfiles-dir))     
+
+;; Font size
+(set-face-attribute 'default nil :height 120)
