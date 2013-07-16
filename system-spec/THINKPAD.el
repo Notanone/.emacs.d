9a44b54a78d3ad0e2f4554f157aab1e0d723bf0d
notanone <notanone@gmail.com>
2013-07-16 21:46:51 +0300

    Source code pro as default font fo ThinkPad

--- system-spec/THINKPAD.el
+++ system-spec/THINKPAD.el
@@ -3,5 +3,7 @@
 ;; Cache folder
 (setq cache-dir "D:/emacs-cache")
 
-;; Font size
-(set-face-attribute 'default nil :height 120)
+;; Font 
+(set-default-font "-outline-Source Code Pro Light-light-normal-normal-mono-*-*-*-*-c-*-iso8859-1")
+(set-face-attribute 'default nil :font "-outline-Source Code Pro Light-light-normal-normal-mono-*-*-*-*-c-*-iso8859-1" :height 120)
+(set-face-font 'default "-outline-Source Code Pro Light-light-normal-normal-mono-*-*-*-*-c-*-iso8859-1")
